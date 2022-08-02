d67640e9a46c047511e9abb753387b7a10efc9be
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-08-02 22:39:49 +0200

    refactor: Remove unused imports

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,43 +1,34 @@
 import React from "react";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { FaUser } from "react-icons/fa";
-import { FiLogIn } from "react-icons/fi";
-import SearchBar from "../SearchBar/SearchBar";
-import { Link, NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import NavIcons from "./NavIcons";
 import NavList from "./NavList";
 const Navbar = ({ searchUpdate }) => {
-    const { user } = useSelector((state) => state.auth);
-
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light sticky-top">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">
-                        Orderat
-                    </Link>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div
-                        className="collapse navbar-collapse"
-                        id="navbarSupportedContent"
-                    >
-                        <NavList />
-                        <NavIcons searchUpdate={searchUpdate} />
-                    </div>
+        <nav className="navbar navbar-expand-lg navbar-light sticky-top">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    Orderat
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <NavList />
+                    <NavIcons searchUpdate={searchUpdate} />
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     );
 };
 
