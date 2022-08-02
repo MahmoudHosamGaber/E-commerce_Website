b98f0986f1097156a396054b3d238bfef129883f
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-08-02 18:16:38 +0200

    Update useForm hook

--- frontend/src/hooks/useForm.js
+++ frontend/src/hooks/useForm.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 export const useForm = (initialState) => {
-  const [values, setValues] = useState(initialState);
+    const [values, setValues] = useState(initialState);
 
-  const handleChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+    const handleChange = (e) => {
+        setValues((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
 
-  return [values, handleChange];
+    return [values, handleChange];
 };
