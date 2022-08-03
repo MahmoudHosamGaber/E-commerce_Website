86d93c503d75c08c7a298582d7b6e5351ea86960
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-08-03 15:22:28 +0200

    refactor: Remove usused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,53 +7,43 @@ import { useState } from "react";
 import { Navbar, Footer } from "./components";
 
 import {
-  UserInfo,
-  Orders,
-  Login,
-  Categories,
-  Products,
-  ProductDetails,
-  SearchPage,
-  Home,
-  FAQs,
-  Cart,
-  Security,
+    UserInfo,
+    Orders,
+    Login,
+    Categories,
+    Products,
+    ProductDetails,
+    SearchPage,
+    Home,
+    FAQs,
+    Cart,
+    Security,
 } from "./pages";
 import Register from "./pages/Log-Reg/Register.js";
 
 const App = () => {
-  const [searchResults, setSearchResults] = useState([]);
-
-  const searchUpdateHandle = (results) => {
-    setSearchResults(results);
-  };
-
-  return (
-    <BrowserRouter>
-      <Navbar searchUpdate={searchUpdateHandle} />
-      <Routes>
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:categoryId" element={<Products />} />
-        <Route path="/productDetails" element={<ProductDetails />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/profile" element={<UserInfo />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/faqs" element={<FAQs />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/profile/security" element={<Security />} />
-
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/SearchPage"
-          element={<SearchPage results={searchResults} />}
-        />
-      </Routes>
-      <ToastContainer />
-      <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:categoryId" element={<Products />} />
+                <Route path="/productDetails" element={<ProductDetails />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/profile" element={<UserInfo />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/faqs" element={<FAQs />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/profile/security" element={<Security />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/SearchPage" element={<SearchPage />} />
+            </Routes>
+            <ToastContainer />
+            <Footer />
+        </BrowserRouter>
+    );
 };
 export default App;
