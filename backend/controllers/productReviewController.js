b391c2fe87456919b38df740f5e84b3dd9a291a8
Muhammad <Muhammad.medhat@gmail.com>
2022-08-05 15:47:45 +0200

    fix: backend product reviews

--- backend/controllers/productReviewController.js
+++ backend/controllers/productReviewController.js
@@ -3,56 +3,56 @@ const mongoose = require("mongoose");
 const ProductReview = require("../model/productReviewModel");
 const Product = require("../model/productModel");
 
-
-// @desc    Add a Review to a product 
+// @desc    Add a Review to a product
 // @route   POST /api/product/:id/review
 // @access  Private
 
 const createReview = asyncHandler(async (req, res) => {
-    const { stars , comment } = req.body;
-    if ( !stars || !comment ) {
-      res.status(400);
-      throw new Error("Please add all fields");
-    }
-    const productId = req.params.id;
-    // Check if product exists
-    const productExists = await Product.findById(productId);
-    if (!productExists) {
-      res.status(400);
-      throw new Error("Product doesn't exists");
-    }
-   
-    // Check if user review exists
-    const reviewExists = await ProductReview.findOne({ userId: req.user.id , productId: productId});
-    if (reviewExists) {
-      res.status(400);
-      throw new Error(" You have already reviewed this product ");
-    }
-    const review = await ProductReview.create({
-      stars,
-      comment,
-      userId : req.user.id,
-      productId : req.params.id
-    });
-    if (review) {
-      res.status(201).json({
-        name: req.user.username,
-        stars: review.stars,
-        comment: review.comment,
-      });
-    } else {
-      res.status(400);
-      throw new Error("Invalid review data");
-    }
+  const { stars, comment } = req.body;
+  if (!stars || !comment) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+  const productId = req.params.id;
+  // Check if product exists
+  const productExists = await Product.findById(productId);
+  if (!productExists) {
+    res.status(400);
+    throw new Error("Product doesn't exists");
+  }
+
+  // Check if user review exists
+  const reviewExists = await ProductReview.findOne({
+    userId: req.user.id,
+    productId: productId,
   });
+  if (reviewExists) {
+    res.status(400);
+    throw new Error(" You have already reviewed this product ");
+  }
+  const review = await ProductReview.create({
+    stars,
+    comment,
+    userId: req.user.id,
+    productId: req.params.id,
+  });
+  if (review) {
+    res.status(201).json({
+      name: req.user.username,
+      stars: review.stars,
+      comment: review.comment,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid review data");
+  }
+});
 
-
- 
-// @desc    Get Product Reviews
-// @route   Get /api/product/:id
-// @access  Public
-
-
+/**
+ * @desc    Get Product Reviews
+ * @route   Get /api/product/:id/reviews
+ * @access  Public
+ */
 
 const getReviews = asyncHandler(async (req, res) => {
   const productId = req.params.id;
@@ -77,7 +77,7 @@ const getReviews = asyncHandler(async (req, res) => {
       },
     },
   ]);
-  //show All the comment andiew of thproduct
+  //show All the comment and reviews of the product
   const reviewsList = await ProductReview.find({ productId: productId });
   if (reviews) {
     res.status(200).json({
@@ -88,9 +88,6 @@ const getReviews = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("No reviews found");
   }
-}
+});
 
-);
-
-
-module.exports = { createReview , getReviews }
+module.exports = { createReview, getReviews };
--- backend/routes/api/productReviewRoutes.js
+++ backend/routes/api/productReviewRoutes.js
@@ -7,6 +7,12 @@ const {
 const { verifyAuth } = require("../../middleware/authMiddleware");
 
 router.post("/:id/review", verifyAuth, createReview);
-router.get("/:id", getReviews);
+
+/**
+ * @desc    Get Product Reviews
+ * @route   Get /api/product/:id/reviews
+ * @access  Public
+ */
+router.get("/:id/reviews", getReviews);
 
 module.exports = router;
