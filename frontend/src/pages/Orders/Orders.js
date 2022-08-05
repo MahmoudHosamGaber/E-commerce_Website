6d49c17afc0eba17d36122472f121c057f0f7f99
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-08-05 21:44:28 +0200

    feat: Add a spinner to show while the app is loading

--- frontend/src/pages/Orders/Orders.js
+++ frontend/src/pages/Orders/Orders.js
@@ -3,35 +3,36 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchOrders } from "../../features/orders/ordersSlice";
 import "./orders.css";
+import Spinner from "../../components/Spinner";
 
 const Orders = () => {
-  const orders = useSelector((state) => state.orders);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchOrders());
-  }, [dispatch]);
-  if (orders.isLoading) {
-    return <div>Loading...</div>;
-  }
+    const orders = useSelector((state) => state.orders);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchOrders());
+    }, [dispatch]);
+    if (orders.isLoading) {
+        return <Spinner />;
+    }
 
-  if (orders.isError) {
+    if (orders.isError) {
+        return (
+            <>
+                <div>{orders.message}</div>;{" "}
+            </>
+        );
+    }
+
     return (
-      <>
-        <div>{orders.message}</div>;{" "}
-      </>
-    );
-  }
-
-  return (
-    <div className="orders_wrapper">
-      <div className="orders_wrapper-content d-flex w-100 p-5">
-        <h1 className="mb-5">My Orders</h1>
-        <div className="order_wrapper-content_items w-75">
-          <OrdersList orders={orders.orders} />
+        <div className="orders_wrapper">
+            <div className="orders_wrapper-content d-flex w-100 p-5">
+                <h1 className="mb-5">My Orders</h1>
+                <div className="order_wrapper-content_items w-75">
+                    <OrdersList orders={orders.orders} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Orders;
