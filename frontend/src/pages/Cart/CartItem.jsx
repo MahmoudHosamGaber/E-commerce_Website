c30ba7073fe997a32249a15d95a21199ef7fc5a0
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-08-06 14:55:13 +0200

    fix: Add the total price to the card page

--- frontend/src/pages/Cart/CartItem.jsx
+++ frontend/src/pages/Cart/CartItem.jsx
@@ -1,35 +1,45 @@
-import {removeItem} from "../../features/cart/cartSlice";
+import { removeItem } from "../../features/cart/cartSlice";
 //import { useEffect } from 'react';
-import {useDispatch} from "react-redux";
-import './Cart.css';
-import {BsFillTrashFill} from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import "./Cart.css";
+import { BsFillTrashFill } from "react-icons/bs";
 //import { toast } from "react-toastify";
 
-const CartItem = ({cartItem}) => {
+const CartItem = ({ cartItem }) => {
+    //const {isError, isSuccess, message} = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
 
-  //const {isError, isSuccess, message} = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
+    let productId = {
+        productId: cartItem.productId,
+    };
+    const deleteItem = ({ productId }) => {
+        dispatch(removeItem({ productId }));
+    };
 
-  let productId = {
-    productId: cartItem.productId,
-  }
-  const deleteItem = ({productId}) => {
-    dispatch(removeItem({productId}));
-  }
- 
-  return (
-    <div className='cart-card' key={cartItem.productId}>
-                    <div className='card-content'>
-                      <div className='product-info'>
-                        <img className="img-fluid" src={cartItem.image} alt="product"></img>
-                        <h3> {cartItem.name} </h3>
-                      </div>
-                        <h4> {cartItem.quantity} </h4>
-                        <h4 className='total-price'>{cartItem.totalPrice}</h4> 
-                        <button className="delete-btn" onClick={() => deleteItem(productId)}><BsFillTrashFill  /></button>
-                    </div>
-                  </div>
-  )
-}
+    return (
+        <div className="cart-card" key={cartItem.productId}>
+            <div className="card-content">
+                <div className="product-info">
+                    <img
+                        className="img-fluid"
+                        src={cartItem.image}
+                        alt="product"
+                    ></img>
+                    <h3> {cartItem.name} </h3>
+                </div>
+                <h4> {cartItem.quantity} </h4>
+                <h4 className="total-price">
+                    {cartItem.price * cartItem.quantity}
+                </h4>
+                <button
+                    className="delete-btn"
+                    onClick={() => deleteItem(productId)}
+                >
+                    <BsFillTrashFill />
+                </button>
+            </div>
+        </div>
+    );
+};
 
-export default CartItem
+export default CartItem;
